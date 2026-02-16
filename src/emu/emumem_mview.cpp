17cd117771ad9a949e32cd8dbe4c611083c0192c
hap <happppp@users.noreply.github.com>
2026-02-16 01:00:49 +0100

    emumem_mview: on switch, do nothing if selected slot is same as before

--- src/emu/emumem_mview.cpp
+++ src/emu/emumem_mview.cpp
@@ -530,6 +530,9 @@ void memory_view::disable()
 
 void memory_view::select(int slot)
 {
+	if (slot < 0)
+		fatalerror("memory_view %s: select of unknown slot %d\n", m_name, slot);
+
 	if (slot != m_cur_slot || m_cur_id == -1) {
 		auto i = m_entry_mapping.find(slot);
 		if (i == m_entry_mapping.end())
@@ -552,7 +555,7 @@ int memory_view::id_to_slot(int id) const
 void memory_view::initialize_from_address_map(offs_t addrstart, offs_t addrend, const address_space_config &config)
 {
 	if (m_config)
-		fatalerror("A memory_view can be present in only one address map.");
+		fatalerror("A memory_view can be present in only one address map.\n");
 
 	m_config = &config;
 	m_addrstart = addrstart;
